f999b0aeef5d57011f1e4e8e3a4772ff4e506f8f
_ipal <malkoleyplay@gmail.com>
2018-06-21 13:40:17 +0300

    test

--- cti_ui/cti_ui/crt_form.cpp
+++ cti_ui/cti_ui/crt_form.cpp
@@ -9,9 +9,9 @@ int main(array<String^>^ args) {
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
 
-	ctiui::crt_form form;
+	ctiui::crt_form ctiF;
 
-	Application::Run(%form);
+	Application::Run(%ctiF);
 
 	return 0;
 }
